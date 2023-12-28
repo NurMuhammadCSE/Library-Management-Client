b535e9a54908c0720273cfc8d35a1544ce55f99d
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2023-12-28 16:57:21 +0600

    Create Sign up Page

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
+import AuthProvider from "./provider/AuthProvider.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-screen-xl mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <HelmetProvider>
+      <AuthProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
